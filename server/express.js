40437f839d59ce8374589ce72012966de42db95e
Thibault <thib-17@orange.fr>
2018-06-08 14:25:10 +0200

    dependencies

--- server/express.js
+++ server/express.js
@@ -1,11 +1,30 @@
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const uuidv4 = require("uuid/v4");
 
 const config = require("./config");
 const app = express();
 
 app.use('/public', express.static(config.express.public_path));
+app.use(bodyParser());
+app.use(cookieParser());
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(config.express.public_path, "login.html"), {}, err => {
+        if (err) {
+            next(err);
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { login } = req.query;
+
+
+    return res.redirect('/');
+});
 
 app.get("/", (req, res) => {
     res.redirect('/' + uuidv4());
